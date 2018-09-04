78fc68ce83ea52ecf363b69d5226af615ffc7ad0
Cody Frederick <cody@sketchdev.io>
2018-09-04 13:05:05 -0500

    checkpoint

--- cli.js
+++ cli.js
@@ -3,8 +3,11 @@
 require('dotenv').config();
 
 const fs = require('fs');
+const path = require('path');
+const figlet = require('figlet');
 const pkg = require('./package.json');
 const program = require('commander');
+const throng = require('throng');
 
 let configFile, config;
 
@@ -15,20 +18,38 @@ program.description('Runs an Express+Wrestler instance using the configuration f
 program.action(config => configFile = config);
 program.parse(process.argv);
 
-if (configFile && fs.existsSync(configFile)) {
-  config = require(configFile);
+if (configFile && typeof configFile === 'string') {
+  if (fs.existsSync(configFile)) {
+    config = require(path.resolve(configFile));
+  } else {
+    console.log(`\nFile [${configFile}] does not exist.`);
+    program.help();
+  }
 }
 
-const express = require('express');
-const logger = require('morgan');
-const wrestler = require('../wrestler');
+const master = () => {
+  figlet('Wrestler', (err, data) => {
+    if (!err) {
+      console.log(data);
+    }
+  });
+};
 
-const PORT = process.env.PORT || 3000;
+const start = (id) => {
+  const express = require('express');
+  const logger = require('morgan');
+  const wrestler = require('wrestler');
 
-const app = express();
-app.set('trust proxy', 1); // trust first proxy
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(wrestler(config));
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+  const PORT = process.env.PORT || 3077;
+
+  const app = express();
+  app.set('trust proxy', 1); // trust first proxy
+  app.use(logger(process.env.LOG_FORMAT || 'dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(wrestler(config));
+  app.listen(PORT, () => console.log(`[${id}] Listening on port ${PORT}...`));
+};
+
+const workers = process.env.WEB_CONCURRENCY || undefined;
+throng({ workers, lifetime: Infinity, start, master });
---
+++ example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  users: true,
+  database: {
+    persistentDataPath: 'data'
+  }
+};
