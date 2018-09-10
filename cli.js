85a7906233ecfb5b80f68569abed5b32bf2d4762
Cody Frederick <cody.frederick@outlook.com>
2018-09-10 11:05:11 -0500

    updating to wrestler 0.5.x specifications

--- cli.js
+++ cli.js
@@ -4,10 +4,12 @@ require('dotenv').config();
 
 const fs = require('fs');
 const path = require('path');
-const figlet = require('figlet');
+const util = require('util');
+const figlet = util.promisify(require('figlet'));
 const pkg = require('./package.json');
 const program = require('commander');
 
+const PORT = process.env.PORT || 3077;
 let configFile, config;
 
 program.version(pkg.version, '-v, --version');
@@ -26,21 +28,25 @@ if (configFile && typeof configFile === 'string') {
   }
 }
 
-figlet('Wrestler', (err, data) => {
-  if (!err) {
-    console.log(data);
+(async () => {
+  try {
+    console.log(await figlet('Wrestler'));
+    console.log('Press ctrl+c to exit');
+
+    const express = require('express');
+    const logger = require('morgan');
+    const wrestler = require('wrestler');
+    const api = await wrestler.setup(config);
+
+    const app = express();
+    app.set('trust proxy', 1); // trust first proxy
+    app.use(logger(process.env.LOG_FORMAT || 'dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(api);
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+  } catch (err) {
+    console.log(err);
   }
-  const express = require('express');
-  const logger = require('morgan');
-  const wrestler = require('wrestler');
+})();
 
-  const PORT = process.env.PORT || 3077;
-
-  const app = express();
-  app.set('trust proxy', 1); // trust first proxy
-  app.use(logger(process.env.LOG_FORMAT || 'dev'));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(wrestler(config));
-  app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
-});
